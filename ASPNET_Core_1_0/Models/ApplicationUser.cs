0b8feaf0f93582d5f156dc9b59830803ad2190eb
SelesGames <aemami99@gmail.com>
2017-06-28 13:50:21 -0700

    initial commit of TradeAssist.Web

---
+++ ASPNET_Core_1_0/Models/ApplicationUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace TradeAssist.Web.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        public bool IsGoogleAuthenticatorEnabled { get; set; }
+        public string GoogleAuthenticatorSecretKey { get; set; }
+    }
+}
---
+++ ASPNET_Core_1_0/Models/ManageViewModels/GoogleAuthenticatorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TradeAssist.Web.Models.ManageViewModels
+{
+    public class GoogleAuthenticatorViewModel
+    {
+        public string SecretKey { get; set; }
+        public string BarcodeUrl { get; set; }
+        public string Code { get; set; }
+    }
+}
